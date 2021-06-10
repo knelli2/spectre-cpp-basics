ec83ccce3a8e2c6ca0836fcc3ed1ac08db4016a6
Kyle Nelli <knelli@caltech.edu>
2021-06-10 08:08:17 -0700

    Nelli Meeting 13

--- MeetingCodes/Meeting13/Nelli_BigScalar.cpp
+++ MeetingCodes/Meeting13/Nelli_BigScalar.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 #include <cstddef>
 #include <gsl/gsl>
 
@@ -7,9 +8,13 @@ class BigScalar {
     public:
     std::vector<double> data{};
 
-    BigScalar(const std::vector<double>& init_data) {
-        data = init_data;
-    };
+    BigScalar(const gsl::not_null<std::vector<double>*> init_data) {
+        data = *init_data;
+    }
+
+    BigScalar(std::vector<double>&& init_data) {
+        data = std::move(init_data);
+    }
 };
 
 int main() {
@@ -19,9 +24,14 @@ int main() {
     for (double& elem : test_vector) {
         elem = 0.0;
     }
-    BigScalar big_scalar{test_vector};
+    // BigScalar big_scalar{gsl::make_not_null(&test_vector)};
+    BigScalar big_scalar{std::move(test_vector)};
     
-    std::cout << big_scalar.data[size/2] << "\n";
+    std::cout << gsl::at(big_scalar.data, size / 2) << "\n";
+    
+    double x = std::numeric_limits<double>::signaling_NaN();
+    x = big_scalar.data[4];
+    std::cout << x+ 7 << "\n";
 
     return 0;
 }
